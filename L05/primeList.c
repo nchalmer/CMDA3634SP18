e5c8540e05d4c9866b99872cc6bda4d9cc9d0d6a
Noel <nchalmer@vt.edu>
2018-02-07 18:36:53 -0500

    Fixed up the primelist example

--- L05/primeList.c
+++ L05/primeList.c
@@ -20,7 +20,7 @@ void main() {
 
   for (int i=2;i<sqrtN;i++) {
     if (isPrime[i]) { //if i is prime
-      for (int j=i*i;j<N;j++) {
+      for (int j=i*i;j<N;j+=i) {
         isPrime[j] = 0;//set j not prime
       }
     }
@@ -28,7 +28,7 @@ void main() {
 
   // count the number of primes we found 
   int cnt =0;
-  for (int n=0;n<N;n++) {
+  for (int n=2;n<N;n++) {
     if (isPrime[n]) {
       cnt++;
     }
@@ -39,13 +39,16 @@ void main() {
 
   //loop once more and build the list
   cnt =0;
-  for (int n=0;n<N;n++) {
+  for (int n=2;n<N;n++) {
     if (isPrime[n]) {
       primes[cnt++] = n;
     }
   } 
 
   //print out what find 
-  for (int n=0;n<cnt;n++) printf("The %d-th prime is %d\n", n, primes[n]);
+  for (int n=0;n<cnt;n++) printf("The %d-th prime is %d\n", n+1, primes[n]);
   
+  //cleanup
+  free(isPrime);
+  free(primes); 
 }
