54ee3de55af0f932974eb1ce011aa52f69da9c13
Noel <nchalmer@vt.edu>
2018-02-27 15:45:25 -0500

    Adding mpi version of Monte Carlo pi estimate

--- L11/spi.c
+++ L11/spi.c
@@ -2,17 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include "mpi.h"
+
 int main(int argc, char **argv) {
+
+  MPI_Init(&argc,&argv);
+  
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
 
   //need running tallies
   long long int Ntotal=0;
   long long int Ncircle=0;
 
   //seed random number generator
-  double seed = 1.0;
+  double seed = rank;
   srand48(seed);
 
-  for (long long int n=0; n<1000000000;n++) {
+  long long int Ntrials = 10000000/size;
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double startTime = MPI_Wtime();
+
+  for (long long int n=0; n<Ntrials; n++) {
     //gererate two random numbers
     double rand1 = drand48(); //drand48 returns a number between 0 and 1
     double rand2 = drand48();
@@ -25,9 +38,19 @@ int main(int argc, char **argv) {
     Ntotal++;
   }
 
-  double pi = 4.0*Ncircle/ (double) Ntotal;
+  long long int globalNcircle=0;
+  MPI_Allreduce(&Ncircle, &globalNcircle, 1,
+                  MPI_LONG_LONG_INT, MPI_SUM, MPI_COMM_WORLD);
 
-  printf("Our estimate of pi is %f \n", pi);
+  double pi = 4.0*globalNcircle/ (double) (Ntotal*size);
+  
+  MPI_Barrier(MPI_COMM_WORLD);
+  double endTime = MPI_Wtime();  
+
+  if (rank==0) 
+    printf("Our estimate of pi is %f, the runtime was %g \n", pi, endTime-startTime);
+  
+  MPI_Finalize();
 
   return 0;
 }
